a44fd3cc555fba53df725875c4ff7371743fd367
Jason Greer <jasemgreer@gmail.com>
2023-09-05 11:10:35 -0500

    complete

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+        vin: '22222222222222222',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmisson: 'manual',
+    },
+    {
+        vin: '33333333333333333',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 115000,
+        title: 'salvage',
+    },
+    {
+        vin: '44444444444444444',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+    },
+]
+
+exports.seed = function(knex) {
+    return knex('cars')
+    .truncate().then(() => {
+        return knex('cars').insert(cars)
+    }) 
+}
